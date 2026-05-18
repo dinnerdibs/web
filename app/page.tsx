6a984c15a1bd6fa51a5506edcfc475ac40bbd48f
Sunil Shastry <sunil.shastry69@gmail.com>
2026-05-17 22:36:08 -0600

    Build a very basic template page for the landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 export default function Page() {
 	return (
 		<div>
-			<h1>Hello world!</h1>
+			<h1 className="font-heading text-2xl">Hello world!</h1>
+			<h3 className="font-body">Welcome to DinnerDibs!</h3>
 		</div>
 	);
 }
