ae9c0d503919238fdb6a5a9fd9644b652926c806
Sunil Shastry <sunil.shastry69@gmail.com>
2026-05-22 22:46:22 -0600

    Change the styling for the page by a bit and include the link to the /signup route

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,22 @@
+import Link from 'next/link';
+
 export default function Page() {
 	return (
-		<div>
+		<div className="p-5">
 			<h1 className="font-heading text-content-secondary text-2xl">
 				DinnerDibs
 			</h1>
-			<h3 className="font-body">
+			<h3 className="font-body mt-2">
 				Welcome to DinnerDibs! Find and discover your favorite homemade meals
 				from chefs around you
 			</h3>
+
+			<Link
+				href="/signup"
+				className="text-content-tertiary underline underline-offset-2"
+			>
+				Sign Up
+			</Link>
 		</div>
 	);
 }
